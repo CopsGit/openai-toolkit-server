ea94b3515680c4a1f3f9340661fcee9f5c219b1f
copsgit <github@copsking.com>
2022-12-27 02:35:24 -0500

    code chat api

--- src/router/chatRoute.ts
+++ src/router/chatRoute.ts
@@ -5,5 +5,6 @@ import {ChatController} from "../controller/ChatController";
 const chatRouter = Router()
 
 chatRouter.post('/', ChatController.basicChat)
+chatRouter.post('/code', ChatController.codeChat)
 
 export default chatRouter
